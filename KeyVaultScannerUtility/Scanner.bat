66b8f36116ed70b26fce8673af62244646c61a3c
Akash Khandelwal <akashkhandelwal0011@gmail.com>
2021-05-10 11:30:32 +0000

    Update Scanner.bat

--- KeyVaultScannerUtility/Scanner.bat
+++ KeyVaultScannerUtility/Scanner.bat
@@ -1,3 +1,3 @@
 @echo off
 
-powershell.exe -noprofile -NoExit -window minimized -command "&{start-process powershell -ArgumentList '-NoExit -noprofile -window minimized -file "%CD%\KeyVaultscanner.ps1" ' -verb RunAs}"
+pwsh -noprofile -NoExit -window minimized -command "&{start-process pwsh -ArgumentList '-NoExit -noprofile -window minimized -file "%CD%\KeyVaultscanner.ps1" ' -verb RunAs}"
